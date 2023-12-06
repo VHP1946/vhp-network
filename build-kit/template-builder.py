b85f84aca4f29afaf13665786437205e9438e96c
vhc1946 <christianv@vogelheating.com>
2023-12-05 19:04:06 -0600

    IT-97 : some modifications to the templates resource files to get it to go through the builder. template file needs to be tested in AWS

--- build-kit/template-builder.py
+++ build-kit/template-builder.py
@@ -3,10 +3,10 @@ import sys
 
 from anyyml import Loader, Dumper, Tagged, Pairs
 
-print('File to Read ->>>>',sys.argv[1])
-print('File to Write ->>>>',sys.argv[2])
+#print('File to Read ->>>>',sys.argv[1])
+#print('File to Write ->>>>',sys.argv[2])
 
-with open('../staging/vhp-net-launch.yml','r') as f:
+with open('../staging/ready-to-build.yml','r') as f:
     data = yaml.load(f,Loader=Loader)
 
 print('Build Data >>>>>',data)
