61af554e712d39851ca24ca88e9ac215c40bde57
vhc1946 <christianv@vogelheating.com>
2023-12-05 18:00:08 -0600

    updates to builder. small test succeed. moving to larger test

--- build-kit/template-builder.py
+++ build-kit/template-builder.py
@@ -1,32 +1,15 @@
 import yaml
-import os
 import sys
+
+from anyyml import Loader, Dumper, Tagged, Pairs
 
 print('File to Read ->>>>',sys.argv[1])
 print('File to Write ->>>>',sys.argv[2])
 
-class Loader(yaml.SafeLoader):
-
-    def __init__(self, stream):
-
-        self._root = os.path.split(stream.name)[0]
-
-        super(Loader, self).__init__(stream)
-
-    def include(self, node):
-
-        filename = os.path.join(self._root, self.construct_scalar(node))
-
-        with open(filename, 'r') as f:
-            return yaml.load(f, Loader)
-
-
-Loader.add_constructor('!include', Loader.include)
-
-with open('../staging/testfile.yml','r') as f:
-    data = yaml.load(f,Loader)
+with open('../staging/vhp-net-launch.yml','r') as f:
+    data = yaml.load(f,Loader=Loader)
 
 print('Build Data >>>>>',data)
 
 with open('../staging/builds/new-build.yml','w+') as wf:
-    write = yaml.dump(data,wf)
+    write = yaml.dump(data,wf,Dumper = Dumper)
